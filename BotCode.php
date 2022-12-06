af37091acf9eac0d233ae4e648c9d44353cc8ff0
MatveiShlepochenko <114959394+MatveiShlepochenko@users.noreply.github.com>
2022-12-06 19:16:46 +0300

    Update BotCode.php

--- BotCode.php
+++ BotCode.php
@@ -1,1 +1,7 @@
- 
+ <?php
+    const TOKEN = '5642033946:AAGmKknGMHuAEIAGuyWZgdIq7n0Nr_rEwdM';
+    const BASE_URL = 'https://api.telegram.org.bot'.TOKEN.'/';
+
+    //https://api.telegram.org.bot5642033946:AAGmKknGMHuAEIAGuyWZgdIq7n0Nr_rEwdM/setWebhook?url=https://bot/';
+
+    //base url - https://bot/BotCode.php
