f07c103bf30ad3ae479e639f5e439f860f89bf01
MatveiShlepochenko <114959394+MatveiShlepochenko@users.noreply.github.com>
2022-12-06 19:18:39 +0300

    Delete BotCode.php

--- BotCode.php
+++
@@ -1,7 +0,0 @@
- <?php
-    const TOKEN = '5642033946:AAGmKknGMHuAEIAGuyWZgdIq7n0Nr_rEwdM';
-    const BASE_URL = 'https://api.telegram.org.bot'.TOKEN.'/';
-
-    //https://api.telegram.org.bot5642033946:AAGmKknGMHuAEIAGuyWZgdIq7n0Nr_rEwdM/setWebhook?url=https://bot/';
-
-    //base url - https://bot/BotCode.php
